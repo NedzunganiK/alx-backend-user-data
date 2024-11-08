bcf4eb382b47309fe4b1764844ff70741fe509af
NedzunganiK <Nedzunganik8@gmail.com>
2024-11-08 12:41:40 +0200

    Personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
-"""A module for encrypting passwords.
-"""
+""" Encrypting passwords """
 import bcrypt
+from bcrypt import hashpw
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password using a random salt.
+    """
+    Module that expects one string argument name password
+
+    Returns:salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks is a hashed password was formed from the given password.
+    """
+    Checks whether the expected  argument (password)
+    is valid and returns a boolean.
+    Args: hashed_password (bytes): hashed password
+          password (str): password in string
+
+    Return:bool
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
